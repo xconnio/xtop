857c610b425c04f06582091357b389c677585155
Asim Farooq <asim@xconn.io>
2025-11-01 17:39:55 +0500

    implement CLI flags (#9)
    * implement CLI flags
    
    * implement CLI flags

--- cmd/xtop/main.go
+++ cmd/xtop/main.go
@@ -4,12 +4,25 @@ import (
 	"context"
 	"log"
 
+	"github.com/jessevdk/go-flags"
+
 	"github.com/xconnio/xconn-go"
 	"github.com/xconnio/xtop"
 )
 
+type Options struct {
+	URL   string `short:"u" long:"url" description:"WAMP router URL" env:"XTOP_URL" default:"ws://localhost:8080/ws"`
+	Realm string `short:"r" long:"realm" description:"WAMP realm to connect" env:"XTOP_REALM" default:"io.xconn.mgmt"`
+}
+
 func main() {
-	session, err := xconn.ConnectAnonymous(context.Background(), "ws://localhost:8080/ws", "io.xconn.mgmt")
+	var opts Options
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		log.Fatalf("failed to parse flags: %v", err)
+	}
+
+	session, err := xconn.ConnectAnonymous(context.Background(), opts.URL, opts.Realm)
 	if err != nil {
 		log.Fatalf("failed to connect: %v", err)
 	}
